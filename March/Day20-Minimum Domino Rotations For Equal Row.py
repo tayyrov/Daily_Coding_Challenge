e4d103da0e5959beed769ca8a3eeb5a733a72a5a
tayyrov <atayyrov@gmail.com>
2022-03-21 23:11:47 +0100

    Solution for March day-20 is added

---
+++ March/Day20-Minimum Domino Rotations For Equal Row.py
@@ -0,0 +1,27 @@
+"""
+Question Source:Leetcode
+Level: Hard
+Topic: Stack
+Solver: Tayyrov
+Date: 19.03.2022
+"""
+from collections import Counter
+from math import inf
+from typing import List
+
+
+def minDominoRotations(tops: List[int], bottoms: List[int]) -> int:
+    ans = inf
+    top_count = Counter(tops)
+    bottom_count = Counter(bottoms)
+    ln = len(tops)
+    for n in range(1, 7):
+        valid = True
+        for i, j in zip(tops, bottoms):
+            if i != n and j != n:
+                valid = False
+                break
+        if valid:
+            ans = min(ans, ln - max(top_count[n], bottom_count[n]))
+
+    return -1 if ans == inf else ans
