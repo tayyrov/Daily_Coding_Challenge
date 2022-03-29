297e6d0aa1c5cd7bb48da403885a3325539b4c2f
tayyrov <atayyrov@gmail.com>
2022-03-29 23:53:10 +0200

    Solution for March day-28 is added

---
+++ March/Day28-Search in Rotated Sorted Array II.py
@@ -0,0 +1,13 @@
+
+"""
+Question Source:Leetcode
+Level: Medium
+Topic: Binary
+Solver: Tayyrov
+Date: 28.03.2022
+"""
+from typing import List
+
+
+def search(nums: List[int], target: int) -> bool:
+    return target in nums
